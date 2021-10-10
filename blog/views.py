5becd1619708e7de808def9eb5694a9701b96ea3
shaheer <mohamedshaheer08@gmail.com>
2021-10-11 01:52:29 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import ListView,DetailView
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+
+class BlogDetailView(DetailView): # new
+    model = Post
+    template_name = 'blog/post_detail.html'
